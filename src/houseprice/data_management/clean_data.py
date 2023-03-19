963b625a51fc7d2b54d95675f12a24cd7450d9fe
Sugarkhuu <sugarkhuul@gmail.com>
2023-03-20 00:48:01 +0100

    to english

--- src/houseprice/data_management/clean_data.py
+++ src/houseprice/data_management/clean_data.py
@@ -1,7 +1,11 @@
 """Function(s) for cleaning the data set(s)."""
 import pdb
-
+from transliterate import translit
 import numpy as np
+
+# Define a function to transliterate Mongolian Cyrillic to English
+def transliterate_mn(text):
+    return translit(text, 'mn', reversed=True)
 
 
 def clean_data(df):
@@ -41,7 +45,7 @@ def clean_data(df):
     df["number_of_balcony"] = df["number_of_balcony"].astype(str)
 
     # replace 'Tarтryй', which means 'No balcony', with 0
-    df["number_of_balcony"] = np.where(df["number_of_balcony"].str.contains("Tarтryй"), "0 Tarтryй", df["number_of_balcony"])
+    df["number_of_balcony"] = np.where(df["number_of_balcony"].str.contains("Tarтrүй"), "0 Tarтrүй", df["number_of_balcony"])
 
     # extract numbers from the Tarт column and convert to float
     df["number_of_balcony"] = df["number_of_balcony"].str.extract("(\\d+[\\.\\d]*)").astype(float)
@@ -54,6 +58,33 @@ def clean_data(df):
     # filter out values below 10 and above 1000. The number of values dropped is 21
     df = df[(df["area_sq_m"] >= 10) & (df["area_sq_m"] <= 1000)]
 
-    pdb.set_trace()
+    # pdb.set_trace()
+
+    # Apply the function to the 'text' column
+    df['location'] = df['location'].apply(transliterate_mn)
+    df['district'] = df['district'].apply(transliterate_mn)
+    df['title'] = df['title'].apply(transliterate_mn)
+
+    flooring_dict = {'Паркет': 'parquet', 'Ламинат': 'laminate',
+                        'Мод': 'wood', 'Цемент': 'cement',
+                        'Плита': 'tiles', 'Чулуу': 'stone'}
+
+    garage_dict = {'Байгаа': 'yes', 'Байхгүй': 'no'}
+    window_dict = {'Вакум': 'vinyl', 'Модон вакум': 'vinwood',
+                   'Мод': 'wood', 'Төмөр вакум': 'viniron'}
+    door_dict   = {'Бүргэд': 'burged', 'Төмөр': 'iron',
+                   'Төмөр вакум': 'ironvacuum', 'Вакум': 'vacuum',
+                   'Мод': 'wood'}
+    lease_dict  = {'Банкны лизингтэй': 'withlease', 'Лизинггүй': 'nolease',
+                   'Хувь лизингтэй': 'privlease'}
+    prog_dict   = {'Ашиглалтад орсон': 'complete', 'Ашиглалтад ороогүй': 'incomplete'}
+
+    df['flooring_material'] = df['flooring_material'].replace(flooring_dict)
+    df['garage'] = df['garage'].replace(garage_dict)
+    df['window_type'] = df['window_type'].replace(window_dict)
+    df['door_type'] = df['door_type'].replace(door_dict)
+    df['leasing'] = df['leasing'].replace(lease_dict)
+    df['construction_progress'] = df['construction_progress'].replace(prog_dict)
+
 
     return df
