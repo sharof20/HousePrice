1a158514824a3994d20389b5f875d946a3a83fe0
Sugarkhuu <sugarkhuul@gmail.com>
2023-03-20 23:26:07 +0100

    pytask for visual

--- src/houseprice/analysis/task_analysis.py
+++ src/houseprice/analysis/task_analysis.py
@@ -2,10 +2,17 @@
 # # 2. model_selection
 # # # try different models on the data, do cross validation and pick the best model
 
+import pandas as pd
+import pytask
+import plotly.express as px
+import plotly.graph_objects as go
+from houseprice.config import BLD, SRC
 
 
+@pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
+@pytask.mark.produces(BLD  / "plot" / "histogram.png")
+def task_hist(depends_on, produces):
+    df = pd.read_csv(depends_on)
+    fig = px.histogram(df, x="price_m2")
+    fig.write_image(produces)
 
-# @pytask.mark.depends_on(
-#     },
-# @pytask.mark.produces(BLD / "python" / "data" / "house_price_analysis.csv")
-# def task_clean_data_python(depends_on, produces):
