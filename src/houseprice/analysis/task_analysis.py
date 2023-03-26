f594a6f750688ae2b019795f940b7c7395da2a1c
Sugarkhuu <sugarkhuul@gmail.com>
2023-03-27 00:22:49 +0200

    clean figures

--- src/houseprice/analysis/task_analysis.py
+++ src/houseprice/analysis/task_analysis.py
@@ -15,7 +15,7 @@ from houseprice.utilities import save_close_plt
 # sns.set_theme()
 
 @pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
-@pytask.mark.produces(BLD  / "plot" / "toy.png")
+@pytask.mark.produces(BLD  / "plot" / "price_by_location.png")
 def task_toy(depends_on, produces):
     df = pd.read_csv(depends_on)
 
@@ -36,39 +36,55 @@ def task_toy(depends_on, produces):
 
 
 @pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
-@pytask.mark.produces(BLD  / "plot" / "histogram.png")
+@pytask.mark.produces(BLD  / "plot" / "histogram_price.png")
 def task_hist(depends_on, produces):
     df = pd.read_csv(depends_on)
     fig = px.histogram(df, x="price_m2")
     fig.write_image(produces)
 
-
-
 @pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
-@pytask.mark.produces(BLD  / "plot" / "price_flrMat.png")
-def task_price_flrMat(depends_on, produces):
-    # read data from csv
+@pytask.mark.produces(BLD  / "plot" / "histogram_area.png")
+def task_hist(depends_on, produces):
     df = pd.read_csv(depends_on)
-
-    # group the data by flooring material and calculate the mean price per square meter
-    grouped = df.groupby(['flooring_material'])['price_m2'].mean().reset_index()
-
-    # create a bar chart using Plotly Express
-    fig = px.bar(grouped, x='flooring_material', y='price_m2')
-
-    # set the chart title and axis labels
-    fig.update_layout(
-        title='Average Price per Square Meter by Flooring Material',
-        xaxis_title='Flooring Material',
-        yaxis_title='Price per Square Meter'
-    )
-
+    fig = px.histogram(df, x="area_sq_m")
     fig.write_image(produces)
 
 
 
 @pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
-@pytask.mark.produces(BLD  / "plot" / "price_area_dist.png")
+@pytask.mark.produces(BLD  / "plot" / "histogram_price_total.png")
+def task_hist(depends_on, produces):
+    df = pd.read_csv(depends_on)
+    fig = px.histogram(df, x="price")
+    fig.write_image(produces)
+
+
+
+vars = ['number_of_balcony', \
+        'year_of_commissioning','number_of_storeys','apartment_storey', \
+        'number_of_windows','garage','flooring_material', \
+        'window_type','door_type','construction_progress']
+
+for var in vars:
+    @pytask.mark.task
+    @pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
+    @pytask.mark.produces(BLD  / "plot" / f"price_{var}.png")
+    def task_toy(depends_on, produces, myvar = var):
+        df = pd.read_csv(depends_on)
+        grouped = df.loc[:,[myvar, 'price_m2']] \
+            .groupby([myvar]) \
+            .median() \
+            .sort_values(by='price_m2')
+
+        sns.boxplot(x=df[myvar], y=df.price_m2, order=grouped.index)
+        plt.xticks(fontsize=10, rotation=90)
+        plt.subplots_adjust(bottom=0.2)
+
+        save_close_plt(produces)
+
+
+@pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
+@pytask.mark.produces(BLD  / "plot" / "price_area.png")
 def task_price_area_dist(depends_on, produces):
     # read data from csv
     df = pd.read_csv(depends_on)
@@ -76,8 +92,6 @@ def task_price_area_dist(depends_on, produces):
     fig = px.scatter(df, x='area_sq_m', y='price_m2', color='district')
 
     fig.write_image(produces)
-
-
 
 @pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
 @pytask.mark.produces(BLD  / "plot" / "price_year.png")
@@ -89,90 +103,3 @@ def task_price_year(depends_on, produces):
     fig = px.line(grouped, x='year_of_commissioning', y='price_m2')
 
     fig.write_image(produces)
-
-
-
-@pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
-@pytask.mark.produces(BLD  / "plot" / "area_dist.png")
-def task_area_dist(depends_on, produces):
-    # read data from csv
-    df = pd.read_csv(depends_on)
-    # create a histogram using seaborn
-    sns.histplot(x='area_sq_m', data=df, color='red')
-
-    # set the chart title and axis labels
-    plt.title('Distribution of Areas', fontsize=16)
-    plt.xlabel('Area (square meters)', fontsize=12)
-    plt.ylabel('Frequency', fontsize=12)
-
-    # set the font size of the tick labels
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-
-    save_close_plt(produces)
-
-
-
-@pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
-@pytask.mark.produces(BLD  / "plot" / "price_area.png")
-def task_price_area(depends_on, produces):
-    # read data from csv
-    df = pd.read_csv(depends_on)
-
-    # create a scatterplot using seaborn
-    sns.scatterplot(x='price_m2', y='area_sq_m', data=df, color='yellow', edgecolor='blue', s=150)
-
-    # set the chart title and axis labels
-    plt.title('Price per Square Meter vs. Area', fontsize=16)
-    plt.xlabel('Price per Square Meter', fontsize=12)
-    plt.ylabel('Area (square meters)', fontsize=12)
-
-    # set the font size of the tick labels
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-
-    save_close_plt(produces)
-
-
-
-@pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
-@pytask.mark.produces(BLD  / "plot" / "total_price_area.png")
-def task_total_price_area(depends_on, produces):
-    # read data from csv
-    df = pd.read_csv(depends_on)
-
-    # create a scatterplot using seaborn
-    sns.scatterplot(x='area_sq_m', y='price', data=df, color='orange', edgecolor='blue', s=150)
-
-    # set the chart title and axis labels
-    plt.title('Price vs. Area', fontsize=16)
-    plt.xlabel('Area (square meters)', fontsize=12)
-    plt.ylabel('Price (million MNT)', fontsize=12)
-
-    # set the font size of the tick labels
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-
-    save_close_plt(produces)
-
-
-
-@pytask.mark.depends_on(BLD / "data" / "house_price_clean.csv")
-@pytask.mark.produces(BLD  / "plot" / "total_price_dist.png")
-def task_total_price_dist(depends_on, produces):
-    # read data from csv
-    df = pd.read_csv(depends_on)
-
-    # create a histogram using seaborn
-    sns.histplot(x='price', data=df, color='green')
-
-    # set the chart title and axis labels
-    plt.title('Distribution of price', fontsize=16)
-    plt.xlabel('price)', fontsize=12)
-    plt.ylabel('Frequency', fontsize=12)
-
-    # set the font size of the tick labels
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-
-    save_close_plt(produces)
