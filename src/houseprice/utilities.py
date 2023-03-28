20ee6cf71b84dec7b930f5a6cda5a07ebc1f462e
Sugarkhuu <sugarkhuul@gmail.com>
2023-03-28 15:12:00 +0200

    listed the functions in test files

--- src/houseprice/utilities.py
+++ src/houseprice/utilities.py
@@ -7,24 +7,3 @@ def save_close_plt(path):
     figure.set_size_inches(8, 6)
     plt.savefig(path)
     plt.close()
-
-def read_yaml(path):
-    """Read a YAML file.
-
-    Args:
-        path (str or pathlib.Path): Path to file.
-
-    Returns:
-        dict: The parsed YAML file.
-
-    """
-    with open(path) as stream:
-        try:
-            out = yaml.safe_load(stream)
-        except yaml.YAMLError as error:
-            info = (
-                "The YAML file could not be loaded. Please check that the path points "
-                "to a valid YAML file."
-            )
-            raise ValueError(info) from error
-    return out
